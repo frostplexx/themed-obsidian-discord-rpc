445e0c3329618f127224f31928aa348e67172ac6
Mouadhbendjedidi <alfadjr2007@gmail.com>
2026-04-29 16:08:56 +0100

    chore: add flake.nix and shell.nix for dev environment

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "themed Discord RPC Dev Flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs_22
+        nodePackages.npm
+      ];
+    };
+  };
+}
---
+++ shell.nix
@@ -0,0 +1,17 @@
+{
+  description = "themed-obsidian-discord-rpc dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs_22
+        nodePackages.npm
+      ];
+    };
+  };
+}
